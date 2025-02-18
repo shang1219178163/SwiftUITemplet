1d59b92fa69c9d3504339b73e80d5ee75dd7e1ef
Shang <shang1219178163@gmail.com>
2025-02-18 17:19:29 +0800

    add：新增类

---
+++ SwiftUITemplet/page/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  SwiftUITemplet
+//
+//  Created by Bin Shang on 2025/2/18.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("DetailView")
+    }
+}
+
+#Preview {
+    DetailView()
+}
