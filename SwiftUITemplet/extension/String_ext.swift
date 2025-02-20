487565454a5733abe7ed01630425181af7bf538b
Shang <shang1219178163@gmail.com>
2025-02-20 12:14:22 +0800

    feat：新增图片资源

---
+++ SwiftUITemplet/extension/String_ext.swift
@@ -0,0 +1,18 @@
+//
+//  String_ext.swift
+//  SwiftUITemplet
+//
+//  Created by Bin Shang on 2025/2/20.
+//
+
+import SwiftUI
+
+struct String_ext: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    String_ext()
+}
