c02eefc2f5489399f1b30dd72ff7ca8d1dfc41f9
Shang <shang1219178163@gmail.com>
2025-02-27 10:50:49 +0800

    feat：WrapView 重构未 Wrap

---
+++ SwiftUITemplet/page/WrapDemo.swift
@@ -0,0 +1,39 @@
+//
+//  WrapViewDemo.swift
+//  SwiftUITemplet
+//
+//  Created by Bin Shang on 2025/2/27.
+//
+
+import SwiftUI
+
+
+struct WrapDemo: View {
+    var body: some View {
+        ScrollView {
+            Wrap(spacing: 10,
+                        runSpacing: 10,
+                        alignment: .leading) {
+                ForEach(0..<20) { i in
+                    Text("选项_\(i)\("z" * i)")
+                        .padding(EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4))
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .onTapGesture { p in
+                            DDLog("onTapGesture: \(i)")
+                        }
+                }
+            }
+            .padding(16)
+        }
+    }
+}
+
+struct WrapDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        WrapDemo()
+    }
+}
