66e2d6c99cce23e35e1851ddc9a61c1e9c10f23c
Shang <shang1219178163@gmail.com>
2025-03-28 09:08:41 +0800

    feat：Wrap 示例优化代码

--- SwiftUITemplet/page/demo/WrapDemo.swift
+++ SwiftUITemplet/page/demo/WrapDemo.swift
@@ -16,25 +16,12 @@ struct WrapDemo: View {
             ScrollView(.vertical, showsIndicators: true) {
                 
                 VStack(alignment: .leading, spacing: 10, content: {
-                    Wrap(
-                        spacing: 10,
-                        runSpacing: 10,
-                        alignment: .leading
-                    ) {
-                        ForEach(0..<20) { i in
-                            Text("选项_\(i)\("z" * i)")
-                                .padding(EdgeInsets.symmetric(
-                                    horizontal: 8,
-                                    vertical: 4))
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(8)
-                                .onTapGesture { p in
-                                    DDLog("onTapGesture: \(i)")
-                                }
-                        }
-                    }.padding(/*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    buildWrap(alignment: .leading, itemBgColor: Color.blue);
                     
+                    buildWrap(alignment: .center, itemBgColor: Color.green);
+
+                    buildWrap(alignment: .trailing, itemBgColor: Color.red);
+   
                 })
             }
             .navigationBar(title: "\(clsName)")
@@ -43,6 +30,46 @@ struct WrapDemo: View {
   //            }
         }
     }
+    
+    
+    private func buildWrap(
+        alignment: HorizontalAlignment = .leading,
+        itemBgColor: Color = .blue
+    ) -> some View {
+        var enumString = ""
+        switch alignment {
+        case HorizontalAlignment.center:
+            enumString = "HorizontalAlignment.center"
+        case HorizontalAlignment.trailing:
+            enumString = "HorizontalAlignment.trailing"
+        default:
+            enumString = "HorizontalAlignment.leading"
+        }
+        
+        return VStack(alignment: alignment, spacing: 10, content: {
+            Text("\(enumString)").font(.subheadline)
+            Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                alignment: alignment
+            ) {
+                ForEach(0..<10) { i in
+                    Text("选项_\(i)\("z" * i)")
+                        .padding(EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4))
+                        .background(itemBgColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .onTapGesture { p in
+                            DDLog("onTapGesture: \(i)")
+                        }
+                }
+            }.padding(/*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Divider()
+        })
+    }
+    
 }
 
 #Preview {
