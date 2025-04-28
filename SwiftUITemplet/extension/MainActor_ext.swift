e3ea1662b7e3c5b4adf5075626f22ff1b5f21a52
Shang <shang1219178163@gmail.com>
2025-04-28 12:20:03 +0800

    feat：新增文件

---
+++ SwiftUITemplet/extension/MainActor_ext.swift
@@ -0,0 +1,20 @@
+//
+//  MainActor_ext.swift
+//  SwiftUITemplet
+//
+//  Created by Bin Shang on 2025/4/28.
+//
+
+
+extension MainActor {
+    static func runSafely<T: Sendable>(_ block: @MainActor () -> T) throws -> T {
+        if Thread.isMainThread {
+          return MainActor.assumeIsolated { block() }
+        }
+    
+        MainActor.assertIsolated("This method is expected to be called in main thread!")
+        return DispatchQueue.main.sync {
+            MainActor.assumeIsolated { block() }
+        }
+  }
+}
